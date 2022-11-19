fddbd7eaf20e78ff27fe793e79d581541a898b0d
Jose <jsissame3484@universidadean.edu.co>
2022-11-19 15:27:03 -0500

    Interception observer

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -2,8 +2,14 @@ const buttonMenu = document.getElementById('button-menu');
 const menu = document.getElementById('menu');
 const menuList = document.getElementById('menu-list');
 
-const boxes = document.querySelectorAll('.box')
 const list  = document.getElementById('menu-list')
+
+const home = document.getElementById('home')
+const about = document.getElementById('about')
+const skills = document.getElementById('skills')
+const projects = document.getElementById('projects')
+const contact = document.getElementById('contact')
+const boxes = [home, about, skills, contact]
 
 buttonMenu.addEventListener('click', ()=>{
     menu.classList.toggle('show-menu');
@@ -15,55 +21,61 @@ menuList.addEventListener('click', ()=>{
     buttonMenu.classList.remove('toggle');
 })
 
-const selectItem = (numItem) => {
-    for (let i = 0; i < 5; i++) {
-        list.children[i].children[0].classList.remove('item-selected')
-    }
-    list.children[numItem].children[0].classList.add('item-selected')
-}
+// const selectItem = (numItem) => {
+//     for (let i = 0; i < 5; i++) {
+//         list.children[i].children[0].classList.remove('item-selected')
+//     }
+//     list.children[numItem].children[0].classList.add('item-selected')
+// }
 
-const callback = (entries) => {
-    console.log(entries);
-    console.log(entries[entries.length-1].target.id);
-    if(entries[entries.length-1].target.id === "home") {
-        selectItem(0)
-    }
-    if(entries[entries.length-1].target.id === "about") {
-        selectItem(1)
-    }
-    if(entries[entries.length-1].target.id === "skills") {
-        selectItem(2)
-    }
-    if(entries[entries.length-1].target.id === "projects") {
-        selectItem(3)
-    }
-    if(entries[entries.length-1].target.id === "contact") {
-        selectItem(4)
-    }
-}
-const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.8
-}
+// const callback = (entries) => {
+//     console.log(entries);
+//     console.log(entries[entries.length-1].target);
+//     if(entries[entries.length-1].target.id === "home") {
+//         selectItem(0)
+//     }
+//     if(entries[entries.length-1].target.id === "about") {
+//         selectItem(1)
+//     }
+//     if(entries[entries.length-1].target.id === "skills") {
+//         selectItem(2)
+//     }
+//     if(entries[entries.length-1].target.id === "projects") {
+//         selectItem(3)
+//     }
+//     if(entries[entries.length-1].target.id === "contact") {
+//         selectItem(4)
+//     }
+// }
+// const options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.3
+// }
 
-const observer = new IntersectionObserver(callback, options)
-boxes.forEach(elem => observer.observe(elem))
+// const observer = new IntersectionObserver(callback, options)
+// const observerProjects = new IntersectionObserver(callback, {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.5
+// })
+// boxes.forEach(elem => observer.observe(elem))
+// observerProjects.observe(projects)
 
-list.addEventListener('click', (e) => {
-    if (e.target.href.includes('home')) {
-        selectItem(0)
-    }
-    if (e.target.href.includes('about')) {
-        selectItem(1)
-    }
-    if (e.target.href.includes('skills')) {
-        selectItem(2)
-    }
-    if (e.target.href.includes('projects')) {
-        selectItem(3)
-    }
-    if (e.target.href.includes('contact')) {
-        selectItem(4)
-    }
-})
+// list.addEventListener('click', (e) => {
+//     if (e.target.href.includes('home')) {
+//         selectItem(0)
+//     }
+//     if (e.target.href.includes('about')) {
+//         selectItem(1)
+//     }
+//     if (e.target.href.includes('skills')) {
+//         selectItem(2)
+//     }
+//     if (e.target.href.includes('projects')) {
+//         selectItem(3)
+//     }
+//     if (e.target.href.includes('contact')) {
+//         selectItem(4)
+//     }
+// })
