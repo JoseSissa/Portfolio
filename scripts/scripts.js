9648e320d77b769943972ecce3b6890b86c18ed6
Jose <jsissame3484@universidadean.edu.co>
2022-11-19 16:22:32 -0500

    Intersection observer added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,13 +3,7 @@ const menu = document.getElementById('menu');
 const menuList = document.getElementById('menu-list');
 
 const list  = document.getElementById('menu-list')
-
-const home = document.getElementById('home')
-const about = document.getElementById('about')
-const skills = document.getElementById('skills')
-const projects = document.getElementById('projects')
-const contact = document.getElementById('contact')
-const boxes = [home, about, skills, contact]
+const boxes = document.querySelectorAll('.box')
 
 buttonMenu.addEventListener('click', ()=>{
     menu.classList.toggle('show-menu');
@@ -21,61 +15,40 @@ menuList.addEventListener('click', ()=>{
     buttonMenu.classList.remove('toggle');
 })
 
-// const selectItem = (numItem) => {
-//     for (let i = 0; i < 5; i++) {
-//         list.children[i].children[0].classList.remove('item-selected')
-//     }
-//     list.children[numItem].children[0].classList.add('item-selected')
-// }
+const selectedItem = (numItem) => {
+    for (let i = 0; i < 5; i++) {
+        list.children[i].children[0].classList.remove('item-selected')
+    }
+    list.children[numItem].children[0].classList.add('item-selected')
+}
 
-// const callback = (entries) => {
-//     console.log(entries);
-//     console.log(entries[entries.length-1].target);
-//     if(entries[entries.length-1].target.id === "home") {
-//         selectItem(0)
-//     }
-//     if(entries[entries.length-1].target.id === "about") {
-//         selectItem(1)
-//     }
-//     if(entries[entries.length-1].target.id === "skills") {
-//         selectItem(2)
-//     }
-//     if(entries[entries.length-1].target.id === "projects") {
-//         selectItem(3)
-//     }
-//     if(entries[entries.length-1].target.id === "contact") {
-//         selectItem(4)
-//     }
-// }
-// const options = {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.3
-// }
+const callback = (entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            if(entry.target.innerText === "Jose Rafael Sissa Mendoza") { selectedItem(0) }
+            if(entry.target.innerText === "About me") { selectedItem(1) }
+            if(entry.target.innerText === "Skills") { selectedItem(2) }
+            if(entry.target.innerText === "Projects") { selectedItem(3) }
+            if(entry.target.innerText === "Contact") { selectedItem(4) }
+        }
+    })
+}
+if(window.screen.width < 2000) {
+    const options = {
+        root: null,
+        rootMargin: "0px 0px -200px 0px",
+        threshold: 1
+    }
+    
+    const observer = new IntersectionObserver(callback, options)
+    boxes.forEach(elem => observer.observe(elem))
 
-// const observer = new IntersectionObserver(callback, options)
-// const observerProjects = new IntersectionObserver(callback, {
-//     root: null,
-//     rootMargin: '0px',
-//     threshold: 0.5
-// })
-// boxes.forEach(elem => observer.observe(elem))
-// observerProjects.observe(projects)
+    list.addEventListener('click', (e) => {
+        if (e.target.href.includes('home')) { selectedItem(0) }
+        if (e.target.href.includes('about')) { selectedItem(1) }
+        if (e.target.href.includes('skills')) { selectedItem(2) }
+        if (e.target.href.includes('projects')) { selectedItem(3) }
+        if (e.target.href.includes('contact')) { selectedItem(4) }
+    })
+}
 
-// list.addEventListener('click', (e) => {
-//     if (e.target.href.includes('home')) {
-//         selectItem(0)
-//     }
-//     if (e.target.href.includes('about')) {
-//         selectItem(1)
-//     }
-//     if (e.target.href.includes('skills')) {
-//         selectItem(2)
-//     }
-//     if (e.target.href.includes('projects')) {
-//         selectItem(3)
-//     }
-//     if (e.target.href.includes('contact')) {
-//         selectItem(4)
-//     }
-// })
