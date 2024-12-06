ccb73ba53428ea3347612cecee6ddfe51d6d03c7
Jose Sissa <sissamendoza@outlook.com>
2024-12-06 18:29:47 -0500

    First commi

---
+++ src/constants/experience.js
@@ -0,0 +1,34 @@
+export const EXPERIENCE_JOBS = [
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis ex distinctio quidem facilis.',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis ex distinctio quidem facilis.',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis ex distinctio quidem facilis.',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		fecha: '2022 - Presente',
+		empresa: 'Gravital',
+		web: 'https://gravital.co/',
+		cargo: 'Desarrollador Frontend',
+		explicacion_cargo: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis ex distinctio quidem facilis.',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	}
+]
---
+++ src/constants/projects.js
@@ -0,0 +1,34 @@
+export const PROJECTS = [
+	{
+		title: 'Portafolio',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
+		image: 'Desarrollador Frontend',
+        image_src: 'Descripción de la imagen',
+		demo: '',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		title: 'Portafolio',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
+		image: 'Desarrollador Frontend',
+        image_src: 'Descripción de la imagen',
+		demo: '',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		title: 'Portafolio',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
+		image: 'Desarrollador Frontend',
+        image_src: 'Descripción de la imagen',
+		demo: '',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	},
+	{
+		title: 'Portafolio',
+		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
+		image: 'Desarrollador Frontend',
+        image_src: 'Descripción de la imagen',
+		demo: '',
+		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+	}
+]
