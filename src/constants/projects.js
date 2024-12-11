5edf256c376f9b862bf661b0a924e08550c1cf0f
Jose Sissa <sissamendoza@outlook.com>
2024-12-10 20:45:50 -0500

    Projects, info and section

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,34 +1,18 @@
 export const PROJECTS = [
-	{
-		title: 'Portafolio',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
-		image: 'Desarrollador Frontend',
-        image_src: 'Descripción de la imagen',
-		demo: '',
-		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
-	},
-	{
-		title: 'Portafolio',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
-		image: 'Desarrollador Frontend',
-        image_src: 'Descripción de la imagen',
-		demo: '',
-		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
-	},
-	{
-		title: 'Portafolio',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
-		image: 'Desarrollador Frontend',
-        image_src: 'Descripción de la imagen',
-		demo: '',
-		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
-	},
-	{
-		title: 'Portafolio',
-		description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores culpa ut nisi minus libero vitae ipsa velit reiciendis.',
-		image: 'Desarrollador Frontend',
-        image_src: 'Descripción de la imagen',
-		demo: '',
-		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
-	}
+  {
+    title: 'Landing Page, La Velada IV',
+    description: 'Landing page inspirada en la web de La Velada IV, creada con la intención de practicar la maquetación y el uso de Three JS para crear un efecto humo en la página web, siguiendo la guía de Midudev.',
+    image_alt: 'Portada de la Landing Page La Velada IV',
+    image_src: '/images/projects/cover/velada-landing.webp',
+    demo: 'https://astro-khaki-nine.vercel.app/',
+    technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Three JS']
+  },
+  {
+    title: 'Página Premios ESLAND 2024',
+    description: 'Página web de los premios ESLAND 2024, se ha logrado mejorar la experiencia de usuario.',
+    image_alt: 'Portada de la web de los ESLAND 2024',
+    image_src: '/images/projects/cover/esland-web.webp',
+    demo: 'https://astro-khaki-nine.vercel.app/',
+    technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Three JS']
+  }
 ]
