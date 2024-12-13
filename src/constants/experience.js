dccf0b7a9d87ca58e8934f09b0696b3e81e70cd5
Jose Sissa <sissamendoza@outlook.com>
2024-12-13 14:46:14 -0500

    Improving sections

--- src/constants/experience.js
+++ src/constants/experience.js
@@ -1,19 +1,13 @@
 export const EXPERIENCE_JOBS = [
-	{
-		date: 'FEB 2022 - Presente',
-		company: 'Grávital',
-		web: 'https://gravital.co/',
-		position: 'Desarrollador Frontend',
-		summary: [
-			`En mi rol como desarrollador Front-End he colaborado y trabajado con los diferentes equipos de 
-			<a class="font-bold" href="https://novitadiamonds.com/" target="_blank" rel="nofollow noreferrer" title="Web de Novita Diamonds">Novita Diamonds</a> 
-			a través de todas sus sucursales localizadas en diferentes países, creando, actualizando y manteniendo diferentes interfaces de usuario logrando que estas sean más atractivas, funcionales y estén optimizadas en los diferentes dispositivos (Desktop, Tablet, Mobile), entregando excelentes resultados y mejorando el rendimiento de las diferentes webs.`,
-			`Además he actualizado los diferentes diseños para las páginas web principales de los proyectos de 
-			<a class="font-bold" href="https://gravital.co/" target="_blank" rel="nofollow noreferrer" title="Web de Grávital">Grávital</a>,
-			<a class="font-bold" href="https://www.cincot.com/" target="_blank" rel="nofollow noreferrer" title="Web de Cincot">Cincot</a>, 
-			<a class="font-bold" href="https://delartesano.co/" target="_blank" rel="nofollow noreferrer" title="Web delArtesano">DelArtesano</a> y 
-			<a class="font-bold" href="https://vanka.com.co/" target="_blank" rel="nofollow noreferrer" title="Web de Vanka">Vanka</a>.`
-		],
-		technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
-	}
+  {
+    date: 'FEB 2022 - Presente',
+    company: 'Grávital',
+    web: 'https://gravital.co/',
+    position: 'Desarrollador Frontend',
+    summary: [
+      'En mi rol como desarrollador Front-End he colaborado y trabajado con los diferentes equipos de <a class="font-bold" href="https://novitadiamonds.com/" target="_blank" rel="nofollow noreferrer" title="Web de Novita Diamonds">Novita Diamonds</a> a través de todas sus sucursales localizadas en diferentes países, creando, actualizando y manteniendo diferentes interfaces de usuario logrando que estas sean más atractivas, funcionales y estén optimizadas en los diferentes dispositivos (Desktop, Tablet, Mobile), entregando excelentes resultados y mejorando el rendimiento de las diferentes webs.',
+      'Además he actualizado los diferentes diseños para las páginas web principales de los proyectos de <a class="font-bold" href="https://gravital.co/" target="_blank" rel="nofollow noreferrer" title="Web de Grávital">Grávital</a>, <a class="font-bold" href="https://www.cincot.com/" target="_blank" rel="nofollow noreferrer" title="Web de Cincot">Cincot</a>, <a class="font-bold" href="https://delartesano.co/" target="_blank" rel="nofollow noreferrer" title="Web delArtesano">DelArtesano</a> y <a class="font-bold" href="https://vanka.com.co/" target="_blank" rel="nofollow noreferrer" title="Web de Vanka">Vanka</a>.'
+    ],
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Docker']
+  }
 ]
