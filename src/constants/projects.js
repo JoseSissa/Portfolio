c697b07c0be01b9adb76e9fb4589f70853a9071b
Jose Sissa <sissamendoza@outlook.com>
2024-12-13 14:41:53 -0500

    Projects section

--- src/constants/projects.js
+++ src/constants/projects.js
@@ -1,17 +1,25 @@
 export const PROJECTS = [
   {
-    title: 'Landing Page, La Velada IV',
-    description: 'Landing page inspirada en la web de La Velada IV, creada con la intención de practicar la maquetación y el uso de Three JS para crear un efecto humo en la página web, siguiendo la guía de Midudev.',
-    image_alt: 'Portada de la Landing Page La Velada IV',
-    image_src: '/images/projects/cover/velada-landing.webp',
-    demo: 'https://astro-khaki-nine.vercel.app/',
-    technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Three JS']
+    title: 'Página web La Velada IV',
+    description: 'Inspirada en la web de la Velada IV, donde se utilizan transiciones, animaciones, optimización de recursos, sistema de votación con autentificación y base de datos.',
+    image_alt: 'Portada de la web de los ESLAND 2024',
+    image_src: '/images/projects/cover/velada-web.webp',
+    demo: 'https://3s-velada-2024.vercel.app/',
+    technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Typescript', 'Turso']
   },
   {
     title: 'Página Premios ESLAND 2024',
-    description: 'Página web de los premios ESLAND 2024, se ha logrado mejorar la experiencia de usuario.',
+    description: 'Inspirada en la web de los premios ESLAND 2024, se ha logrado optimizar recursos, crear sistema de votación con base de datos y agregar internacionalización (i18n), mostrando la web en Español, Inglés y Catalán.',
     image_alt: 'Portada de la web de los ESLAND 2024',
     image_src: '/images/projects/cover/esland-web.webp',
+    demo: 'https://3s-esland-2024.vercel.app/',
+    technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Typescript', 'Turso']
+  },
+  {
+    title: 'Landing Page, La Velada IV',
+    description: 'Landing page inspirada en la web de La Velada IV, creada con la intención de practicar la maquetación, uso de transiciones, animaciones y el uso de Three JS para crear un efecto humo en la página web.',
+    image_alt: 'Portada de la Landing Page La Velada IV',
+    image_src: '/images/projects/cover/velada-landing.webp',
     demo: 'https://astro-khaki-nine.vercel.app/',
     technologies: ['Astro', 'Tailwind CSS', 'JavaScript', 'Three JS']
   }
